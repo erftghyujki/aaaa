b640b6bf74018d890670bf25be88d14afc913d1b
erftghyujki <a.tackoy@yandex.ru>
2025-02-14 04:09:59 +0700

    asd asd

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import StartPage from './StartPage';
+import MessagePage from './MessagePage';
+import FinalPage from './FinalPage';
+
+const App = () => {
+  const [page, setPage] = useState(0);
+
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+
+  return (
+    <div style={{ width: '100%' }}>
+      {page === 0 && <StartPage nextPage={nextPage} />}
+      {page === 1 && <MessagePage nextPage={nextPage} />}
+      {page === 2 && <FinalPage />}
+    </div>
+  );
+};
+
+export default App;
